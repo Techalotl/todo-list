1ad9bbb5222ee2e89b5a06c0c869f44fff84eaab
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2025-09-04 16:02:15 -0600

    Change function openTaskDialog

--- src/pages/new-task.js
+++ src/pages/new-task.js
@@ -7,7 +7,7 @@ const { lightFormat } = require('date-fns');
 export const todayDate = lightFormat(new Date (), 'yyyy-MM-dd');
 const { addDays } = require("date-fns");
 const tomorrowDate = lightFormat(new Date (addDays(new Date (), 1)), 'yyyy-MM-dd');
-const nextWeek = lightFormat(new Date (addDays(new Date (), 7)), 'yyyy-MM-dd');
+const nextWeekDate = lightFormat(new Date (addDays(new Date (), 7)), 'yyyy-MM-dd');
 const mainContainer = document.querySelector('#main-container');
 
 class Task {
@@ -27,43 +27,46 @@ class Task {
 
 const example1 = new Task ('Read email', 'So I can clean my inbox', todayDate, '#3', 'Let us stop procrastinating this, please.', '', false);
 const example2 = new Task ('Take Sparks to the vet', 'What the title says', tomorrowDate, '#1', 'He needs his last vaccine. Also remember to ask about the weird turd he produce.', '', false);
-const example3 = new Task ('Call Simon', 'Need to confirm his assistance', nextWeek,'#2', '', '', true);
+const example3 = new Task ('Call Simon', 'Need to confirm his assistance', nextWeekDate,'#2', '', '', true);
 tasks.push(example1, example2, example3);
 
 export function openTaskDialog () {
   const taskDialog = document.querySelector('#task-dialog');
   const cancelButton = document.querySelector('#cancel-task');
-  const confirmTask = document.querySelector('#confirm-task');
+  const confirmButton = document.querySelector('#confirm-task');
   taskDialog.showModal();
-  cancelButton.addEventListener('click', () => {
-    taskDialog.close();
-  });
-  confirmTask.addEventListener('click', () => {
-    const taskTitle = document.querySelector('#title');
-    const taskDescription = document.querySelector('#description');
-    const taskDate = document.querySelector('#due-date');
-    const taskPriority = document.querySelector('#priority');
-    const taskNotes = document.querySelector('#notes');
-    const taskProject = document.querySelector('#project');
-    const taskForm = document.querySelector('#task-form');
-    if (taskTitle.value === '' && taskDescription.value === '' && taskDate.value === '') {
-      taskForm.reset();
-      return
-    } else {
-      const task = new Task (taskTitle.value || 'Untitled task', taskDescription.value,
-        taskDate.value || todayDate, taskPriority.value, taskNotes.value, taskProject.value, false
-      )
-      tasks.push(task);
-      if (taskProject.value === '') {
-        printTask();
-        taskForm.reset();
-      } else {
-        printProject();
-        taskForm.reset();
-        return
-      }
-    }
+  cancelButton.addEventListener('click', () => {taskDialog.close();});
+  confirmButton.addEventListener('click', () => {
+    checkTaskForm();
   })
+}
+
+function checkTaskForm() {
+  const taskTitle = document.querySelector('#title');
+  const taskDescription = document.querySelector('#description');
+  const taskDate = document.querySelector('#due-date');
+  const taskPriority = document.querySelector('#priority');
+  const taskNotes = document.querySelector('#notes');
+  const taskProject = document.querySelector('#project');
+  const taskForm = document.querySelector('#task-form');
+  if (taskTitle.value === '' && taskDescription.value === '' && taskDate.value === '') {
+    taskForm.reset();
+  } else {
+    addNewTask(taskTitle.value || 'Untitled task', taskDescription.value,
+    taskDate.value || todayDate, taskPriority.value, taskNotes.value, taskProject.value);
+    if (taskProject.value === '') {
+      printTask();
+      taskForm.reset();
+    } else {
+      printProject();
+      taskForm.reset();
+    }
+  }
+}
+
+function addNewTask (title, description, date, priority, notes, toProject) {
+  const task = new Task (title, description, date, priority, notes, toProject, false);
+  tasks.push(task);
 }
 
 //Delete task
@@ -71,6 +74,6 @@ mainContainer.addEventListener('click', (e) => {
   if (e.target.classList[0] === 'trash-icon') {
     const project = tasks[e.target.classList[1]].toProject;
     tasks.splice(e.target.classList[1], 1);
-      printTask(project);
+    printTask(project);
   }
 })
